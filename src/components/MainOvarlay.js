b8a80754ea7d9e80852a587057b3c6726275fcaa
amirMohammad <amyrmhmdahmdy179@gmail.com>
2025-02-13 22:24:20 -0800

    split component into more smaller components

--- src/components/MainOvarlay.js
+++ src/components/MainOvarlay.js
@@ -3,74 +3,7 @@ export default function MainOvarlay() {
     <>
       <div className="main-ovarlay center">
         <div className="ovarlay-hero-img-box">
-          <div className="hero-slider">
-            <div className="slider">
-              <div className="slide">
-                <img src="images/image-product-1.jpg" alt="Photo 1" />
-              </div>
-              <div className="slide">
-                <img src="images/image-product-2.jpg" alt="Photo 2" />
-              </div>
-              <div className="slide">
-                <img src="images/image-product-3.jpg" alt="Photo 3" />
-              </div>
-              <div className="slide">
-                <img src="images/image-product-4.jpg" alt="Photo 4" />
-              </div>
-            </div>
-            <button
-              className="slider__btn slider__btn--left"
-              aria-label="left align"
-            >
-              <svg
-                className="btn-right"
-                viewBox="0 0 12 18"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  className="arrow-icon"
-                  d="M11 1 3 9l8 8"
-                  stroke="#1D2026"
-                  strokeWidth="3"
-                  fill="none"
-                  fillRule="evenodd"
-                />
-              </svg>
-            </button>
-            <button
-              className="slider__btn slider__btn--right"
-              aria-label="right align"
-            >
-              <svg
-                className="btn-right"
-                viewBox="0 0 13 18"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  className="arrow-icon"
-                  d="m2 1 8 8-8 8"
-                  stroke="#1D2026"
-                  strokeWidth="3"
-                  fill="none"
-                  fillRule="evenodd"
-                />
-              </svg>
-            </button>
-            <button className="slider-delete">
-              <svg
-                id="cross-icon-p"
-                viewBox="0 0 14 15"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  id="cross-icon"
-                  d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
-                  fill="#69707D"
-                  fillRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
+          <ActiveSlide />
 
           <div className="ovarlay-operation-tab-box center">
             <img
@@ -102,5 +35,82 @@ export default function MainOvarlay() {
       </div>
       <div className="bg"></div>
     </>
+  );
+}
+
+function ActiveSlide() {
+  return (
+    <div className="hero-slider">
+      <SliderImage />
+
+      <button className="slider__btn slider__btn--left" aria-label="left align">
+        <svg
+          className="btn-right"
+          viewBox="0 0 12 18"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            className="arrow-icon"
+            d="M11 1 3 9l8 8"
+            stroke="#1D2026"
+            strokeWidth="3"
+            fill="none"
+            fillRule="evenodd"
+          />
+        </svg>
+      </button>
+      <button
+        className="slider__btn slider__btn--right"
+        aria-label="right align"
+      >
+        <svg
+          className="btn-right"
+          viewBox="0 0 13 18"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            className="arrow-icon"
+            d="m2 1 8 8-8 8"
+            stroke="#1D2026"
+            strokeWidth="3"
+            fill="none"
+            fillRule="evenodd"
+          />
+        </svg>
+      </button>
+      <button className="slider-delete">
+        <svg
+          id="cross-icon-p"
+          viewBox="0 0 14 15"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            id="cross-icon"
+            d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+            fill="#69707D"
+            fillRule="evenodd"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+function SliderImage() {
+  return (
+    <div className="slider">
+      <div className="slide">
+        <img src="images/image-product-1.jpg" alt="Photo 1" />
+      </div>
+      <div className="slide">
+        <img src="images/image-product-2.jpg" alt="Photo 2" />
+      </div>
+      <div className="slide">
+        <img src="images/image-product-3.jpg" alt="Photo 3" />
+      </div>
+      <div className="slide">
+        <img src="images/image-product-4.jpg" alt="Photo 4" />
+      </div>
+    </div>
   );
 }
